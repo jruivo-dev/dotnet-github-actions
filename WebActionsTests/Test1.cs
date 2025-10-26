260718db2541d51be9eab5eafb91237beed4b8ef
ruivo <ruivo@ruivos-MacBook-Pro.local>
2025-10-26 18:26:52 +0000

    initial commit

---
+++ WebActionsTests/Test1.cs
@@ -0,0 +1,17 @@
+﻿namespace WebActionsTests;
+
+[TestClass]
+public sealed class Test1
+{
+    [TestMethod]
+    public void TestMethod1()
+    {
+        Assert.AreEqual(2, 1 + 1);
+    }
+
+    [TestMethod]
+    public void TestMethod2()
+    {
+        Assert.AreEqual(3, 1 + 2);
+    }
+}
