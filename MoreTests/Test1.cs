47fbfc0f2cab2c7fd5a97d2f4784c743156649f2
ruivo <ruivo@ruivos-MacBook-Pro.local>
2025-10-26 19:13:31 +0000

    .

---
+++ MoreTests/Test1.cs
@@ -0,0 +1,15 @@
+﻿namespace MoreTests;
+
+[TestClass]
+public sealed class TestsThatAreCool
+{
+    [TestMethod]
+    public void LolTest()
+    {
+    }
+
+    [TestMethod]
+    public void LolTest2()
+    {
+    }
+}
